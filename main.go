ea8dc5cfb05e3fb04810cdd4cbca75a7fb346820
phustly <natsume.pcuse75@gmail.com>
2017-09-05 09:39:32 +0900

    [Fix] os.

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"os"
 	"log"
+	"os"
 
-	"github.com/nlopes/slack"
 	"github.com/kelseyhightower/envconfig"
+	"github.com/nlopes/slack"
 )
 
 type envConfig struct {
 	BotToken string `envconfig:"BOT_TOKEN" required:"true"`
 }
 
-func run(api *slack.Client) int {
-	rtm := api.NewRTM()
-	go rtm.ManageConnection()
+func run(args []string) int {
+	var env envConfig
+	if err := envconfig.Process("", &env); err != nil {
+		log.Printf("[ERROR] Failed to process env var: %s", err)
+		return 1
+	}
+	log.Printf("[INFO] Start slack event listening")
+	client := slack.New(env.BotToken)
 
+	rtm := client.NewRTM()
+	go rtm.ManageConnection()
 	for {
 		select {
 		case msg := <-rtm.IncomingEvents:
@@ -25,7 +32,7 @@ func run(api *slack.Client) int {
 			case *slack.MessageEvent:
 				log.Print("message, event")
 				rtm.SendMessage(rtm.NewOutgoingMessage("ok!", ev.Channel))
-			
+
 			case *slack.InvalidAuthEvent:
 				log.Print("Invalid credentials")
 				return 1
@@ -35,13 +42,5 @@ func run(api *slack.Client) int {
 }
 
 func main() {
-	var env envConfig
-	if err := envconfig.Process("", &env); err != nil {
-		log.Printf("[ERROR] Failed to process env var: %s", err)
-		os.Exit(1)
-	}
-	log.Printf("[INFO] Start slack event listening")
-
-	api := slack.New(env.BotToken)
-	os.Exit(run(api))
+	os.Exit(run(os.Args[1:]))
 }
