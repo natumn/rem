e8dbae9506fa07f8070c4157464730913cd500f3
phustly <natsume.pcuse75@gmail.com>
2017-09-06 20:08:24 +0900

    [Add]check going to work

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/kelseyhightower/envconfig"
 	"github.com/nlopes/slack"
@@ -28,10 +29,14 @@ func run(args []string) int {
 		case msg := <-rtm.IncomingEvents:
 			switch ev := msg.Data.(type) {
 			case *slack.HelloEvent:
-				log.Print("hello, event")
+				log.Print("[INFO] ")
 			case *slack.MessageEvent:
-				log.Print("message, event")
-				rtm.SendMessage(rtm.NewOutgoingMessage("ok!", ev.Channel))
+				log.Print("[INFO]get message")
+				if ev.Text == "出勤" {
+					log.Print("[INFO]get going to work event")
+					st := time.Now()
+					rtm.SendMessage(rtm.NewOutgoingMessage("出勤を確認しました。\n今日もお仕事頑張ってください！",ev.Channel))
+				}
 
 			case *slack.InvalidAuthEvent:
 				log.Print("Invalid credentials")
