f50254259649292a92ce3cbbb3e6b1ae4c665060
phustly <natsume.pcuse75@gmail.com>
2017-09-16 14:45:16 +0900

    [Add] fix .envrc and add measure one day working time and ticker

--- main.go
+++ main.go
@@ -13,8 +13,15 @@ type envConfig struct {
 	BotToken string `envconfig:"BOT_TOKEN" required:"true"`
 }
 
+type workingTime struct {
+	startWorking  time.Time
+	finishWorking time.Time
+	weekTotal time.Duration
+}
+
 func run(args []string) int {
 	var env envConfig
+	var wt workingTime
 	if err := envconfig.Process("", &env); err != nil {
 		log.Printf("[ERROR] Failed to process env var: %s", err)
 		return 1
@@ -24,6 +31,7 @@ func run(args []string) int {
 
 	rtm := client.NewRTM()
 	go rtm.ManageConnection()
+	t := time.NewTicker(10 * time.Second)
 	for {
 		select {
 		case msg := <-rtm.IncomingEvents:
@@ -33,15 +41,24 @@ func run(args []string) int {
 			case *slack.MessageEvent:
 				log.Print("[INFO]get message")
 				if ev.Text == "出勤" {
-					log.Print("[INFO]get going to work event")
-					st := time.Now()
-					rtm.SendMessage(rtm.NewOutgoingMessage("出勤を確認しました。\n今日もお仕事頑張ってください！",ev.Channel))
+					wt.startWorking = time.Now()
+					log.Println("[INFO]start working:", wt.startWorking)
+					rtm.SendMessage(rtm.NewOutgoingMessage("出勤を確認しました。\n今日もお仕事頑張ってください！", ev.Channel))
 				}
-
+				if ev.Text == "退勤" {
+					wt.finishWorking = time.Now()
+					log.Println("[INFO]finish working:", wt.finishWorking)
+					totalTime := wt.finishWorking.Sub(wt.startWorking)
+					log.Println("[INFO]total time:",totalTime.String())
+					rtm.SendMessage(rtm.NewOutgoingMessage("退勤を確認しました。\n今日働いた時間は"+totalTime.String()+"です。\nお疲れ様でした！", ev.Channel))
+					wt.weekTotal += totalTime
+				}
 			case *slack.InvalidAuthEvent:
 				log.Print("Invalid credentials")
 				return 1
 			}
+		case <-t.C:
+			log.Println("[INFO]working time.ticker 10s...")
 		}
 	}
 }
