24c2ed8d115cd28e0f1bae23426760baac90ee26
natumn <natsume.pcuse75@gmail.com>
2018-01-06 16:41:28 +0900

    [Add] run.go

--- main.go
+++ main.go
@@ -1,67 +1,6 @@
 package main
 
-import (
-	"log"
-	"os"
-	"time"
-
-	"github.com/kelseyhightower/envconfig"
-	"github.com/nlopes/slack"
-)
-
-type envConfig struct {
-	BotToken string `envconfig:"BOT_TOKEN" required:"true"`
-}
-
-type workingTime struct {
-	startWorking  time.Time
-	finishWorking time.Time
-	weekTotal time.Duration
-}
-
-func run(args []string) int {
-	var env envConfig
-	var wt workingTime
-	if err := envconfig.Process("", &env); err != nil {
-		log.Printf("[ERROR] Failed to process env var: %s", err)
-		return 1
-	}
-	log.Printf("[INFO] Start slack event listening")
-	client := slack.New(env.BotToken)
-
-	rtm := client.NewRTM()
-	go rtm.ManageConnection()
-	t := time.NewTicker(10 * time.Second)
-	for {
-		select {
-		case msg := <-rtm.IncomingEvents:
-			switch ev := msg.Data.(type) {
-			case *slack.HelloEvent:
-				log.Print("[INFO] ")
-			case *slack.MessageEvent:
-				log.Print("[INFO]get message")
-				if ev.Text == "出勤" {
-					wt.startWorking = time.Now()
-					log.Println("[INFO]start working:", wt.startWorking)
-					rtm.SendMessage(rtm.NewOutgoingMessage("出勤を確認しました。\n今日もお仕事頑張ってください！", ev.Channel))
-				}
-				if ev.Text == "退勤" {
-					wt.finishWorking = time.Now()
-					log.Println("[INFO]finish working:", wt.finishWorking)
-					totalTime := wt.finishWorking.Sub(wt.startWorking)
-					log.Println("[INFO]total time:",totalTime.String())
-					rtm.SendMessage(rtm.NewOutgoingMessage("退勤を確認しました。\n今日働いた時間は"+totalTime.String()+"です。\nお疲れ様でした！", ev.Channel))
-					wt.weekTotal += totalTime
-				}
-			case *slack.InvalidAuthEvent:
-				log.Print("Invalid credentials")
-				return 1
-			}
-		case <-t.C:
-			log.Println("[INFO]working time.ticker 10s...")
-		}
-	}
-}
+import "os"
 
 func main() {
 	os.Exit(run(os.Args[1:]))
--- run.go
+++ run.go
@@ -1,27 +1,70 @@
 package main
 
 import (
-	"github.com/nlopes/slack"
 	"log"
+	"strings"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/nlopes/slack"
+	gentleman "gopkg.in/h2non/gentleman.v2"
 )
 
-func run(api *slack.Client) int {
-	rtm := api.NewRTM()
-	go rtm.ManageConnection()
+type envConfig struct {
+	BotToken string `envconfig:"BOT_TOKEN" required:"true"`
+}
 
+type workingTime struct {
+	startWorking  time.Time
+	finishWorking time.Time
+	weekTotal     time.Duration
+}
+
+func run(args []string) int {
+	cli := gentleman.New()
+	var env envConfig
+	var wt workingTime
+	if err := envconfig.Process("", &env); err != nil {
+		log.Printf("[ERROR] Failed to process env var: %s", err)
+		return 1
+	}
+
+	log.Printf("[INFO] Start slack event listening")
+	slackCli := slack.New(env.BotToken)
+
+	rtm := slackCli.NewRTM()
+	go rtm.ManageConnection()
+	weekAlert := time.NewTicker(168 * time.Hour)
 	for {
 		select {
 		case msg := <-rtm.IncomingEvents:
 			switch ev := msg.Data.(type) {
 			case *slack.HelloEvent:
-				log.Print("hello, event")
+				log.Print("[INFO]working bot...")
 			case *slack.MessageEvent:
-				log.Print("message, event")
-				rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", ev.Channel))
+				log.Print("[INFO]get message")
+				if strings.Contains(ev.Text, "出勤ed") {
+					setToggl()
+					rtm.SendMessage(rtm.NewOutgoingMessage("出勤を確認しました。\n今日もお仕事頑張ってください！", ev.Channel))
+				}
+				if strings.Contains(ev.Text, "退勤ed") {
+					// endToggl()
+					rtm.SendMessage(rtm.NewOutgoingMessage("退勤を確認しました。\n今日働いた時間は です!\nお疲れ様でした。", ev.Channel))
+				}
+				if ev.Text == "todo" {
+					// ttl := getTodayTodo()
+					rtm.SendMessage(rtm.NewOutgoingMessage("今日のtodoは", ev.Channel))
+				}
+				if ev.Text == "今日のプログラミングの時間" {
+
+				}
 			case *slack.InvalidAuthEvent:
 				log.Print("Invalid credentials")
 				return 1
 			}
+		case <-weekAlert.C:
+			log.Println("[INFO]weekend alert")
+			rtm.SendMessage(rtm.NewOutgoingMessage("今週もお疲れ様です。", "test_natsumen"))
 		}
 	}
 }
